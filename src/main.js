502a47c115e2d737d738efa33f92fba221805085
Lucio <lucio.schenkel@acad.pucrs.br>
2021-07-26 21:26:54 -0300

    implements base components auto-registering

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store';
 import VeeValidatePlugin from './includes/validation';
 import { auth } from './includes/firebase';
 import Icon from './directives/icon';
+import GlobalComponents from './includes/_globals';
 import './assets/tailwind.css';
 import './assets/main.css';
 import i18n from './includes/i18n';
@@ -19,6 +20,7 @@ auth.onAuthStateChanged(() => {
     app.use(store);
     app.use(router);
     app.use(VeeValidatePlugin);
+    app.use(GlobalComponents);
     app.directive('icon', Icon);
 
     app.mount('#app');
